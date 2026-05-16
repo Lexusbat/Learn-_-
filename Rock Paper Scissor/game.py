b9762dd82ceb7d58b262a2e8d46388bd0e3cdb19
Lexusbat <skye.lexus205@gmail.com>
2026-05-16 20:06:06 +0200

    Additonal changes

--- Rock Paper Scissor/game.py
+++ Rock Paper Scissor/game.py
@@ -13,20 +13,25 @@ print("Shoot!")
 if choice == choice_pc:
     print('It"s a tie')
 elif choice == 'rock':
+    print(f"Computer chose {choice_pc}")
     if choice_pc == 'scissors':
         print("You win! Rock crushes scissors.")
     else :
         print("You lose! Paper covers rock.")
 elif choice == 'paper':
-    if choice_pc == 'rock':paper
+    print(f"Computer chose {choice_pc}")
+    if choice_pc == 'rock':
         print("You win! Paper covers rock.")
     else :
         print("You lose! Scissors cut paper.")
 elif choice == 'scissors':
+    print(f"Computer chose {choice_pc}")
     if choice_pc == 'paper':
         print("You win! Scissors cut paper.")
     else :
         print("You lose! Rock crushes scissors.")
+else:
+    print("Invalid choice. Please choose rock, paper, or scissors.")
 
 breakpoint()
 print("Would you like to play the game again? Y/N")
