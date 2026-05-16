ca9c4a469549d00c30080d07641283e58a1e3f66
Lexusbat <skye.lexus205@gmail.com>
2026-05-16 19:30:08 +0200

    Fuirst creation of code

---
+++ Rock Paper Scissor/game.py
@@ -0,0 +1,10 @@
+choice = input("Enter your choice (rock, paper, scissors): ")
+for stagesin choice:
+      if stagesin == "rock":
+        print("You chose rock!")
+    elif stagesin == "paper":
+        print("You chose paper!")
+    elif stagesin == "scissors":
+        print("You chose scissors!")
+    else:
+        print("Invalid choice. Please choose rock, paper, or scissors.")
