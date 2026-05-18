a67ea99f3addea52c3b02b6e4fb2c2fb772479f1
Lexusbat <skye.lexus205@gmail.com>
2026-05-18 17:17:35 +0200

    start vfrom scratch

--- Note Web Tutorial/website/main.py
+++
@@ -1,9 +0,0 @@
-from website import create_app 
-
-app = create_app()
-
-if __name__ == '__main__':  #Only if we run this file, not import this file 
-    app.run(debug=True)  #Will excecute this line
-
-#To run a flask application,startup a webserver, any changes to python code -
-#Will automatically rerun the webserver (line 6)
--- Note Web Tutorial/website/templates/_init_.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'LEXBATS'
-
-    return app
-#Created a flask application 
-#Initialized it's secret key
