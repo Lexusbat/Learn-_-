19772e5758d6087c9364134b9510d774622d4aba
Lexusbat <skye.lexus205@gmail.com>
2026-05-25 15:53:15 +0200

    Initial coding

--- Password Generator/code.py
+++ Password Generator/code.py
@@ -1,1 +1,23 @@
 #Loops and Lists
+
+print("Welcome to the password generator")
+letters = input("How many letters would you like?")
+print(letters) # string
+symbols = input("How many symbols would you like?")
+print(symbols) # string
+numbers = input("How many numbers would you like")
+print(numbers) # string
+
+letters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+ 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+symbols_list = [
+    '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.',
+    '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+    '{', '|', '}', '~']
+
+numbers_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
