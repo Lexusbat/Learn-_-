265a8b0759954b8e918df8f52f23aa1d2b28eb71
Lexusbat <skye.lexus205@gmail.com>
2026-05-29 13:12:29 +0200

    additonal changes - functional

--- HM Shell/code.py
+++ HM Shell/code.py
@@ -2,10 +2,20 @@ import random
 
 alph_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 word_list = ["apple", "aardvark", "strawberry"]
-correct = 0
+string_list = []
+string = ""
+count = 0
+flag = 0
 chosen_word = random.choice(word_list)
 print(chosen_word)
- 
+for underscore in range(0,len(chosen_word)):
+    string_list.append("_")
+
+for underrscore in string_list:
+    string += "_"
+print(string)
+
+
 
 user_letter = input("Type a letter to guess word").lower()
 if user_letter not in alph_list:
@@ -13,10 +23,17 @@ if user_letter not in alph_list:
     user_letter = input("Type a letter to guess word").lower()
 else: 
     for char in chosen_word:
+        count += 1
         if user_letter == char:
-            print("Right")
-        else:
-            print("Wrong")
+             flag += 0
+             string_list.insert(count-1,user_letter)       
+             for remove in range(flag + 1):
+                 string_list.pop()
+        
 
+#print(string_list) 
+string =""
+for char in string_list:
+    string += char
+print(string)
 
-    
