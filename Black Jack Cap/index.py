0f006612da085ed492686fcdda9182042f20dbff
Lexusbat <skye.lexus205@gmail.com>
2026-07-01 11:16:01 +0200

    Additional code, function and comments

--- Black Jack Cap/index.py
+++ Black Jack Cap/index.py
@@ -2,86 +2,112 @@ import random
 cards = ["11","2","3","4","5","6","7","8","9","10","10","10","10"]
 pc_cards = []
 user_cards = []
+Ace = False # Stays 11, if True value is 1
+global answer 
 
-def game():
+def game_stand(Ace):
     total_pc = 0
     total_user =0
+    answer = ""
+# Stand
+    for k in range(2):
+     if user_cards[k] == "11":
+      if Ace == True: 
+        user_cards[k] = "1"
+      else: 
+        Ace == False
+
+# Add 2 cards fro user together and add 3 cards from dealer. 
     for i in range(3):
       total_pc += int(pc_cards[i])
-    for k in range(3):
+    for k in range(2):
       total_user += int(user_cards[i])
+
+# Check who has the closest to 21 or bust
     if (total_user > total_pc) and (total_user <= 21):
-      answer =  print("You win")
-    elif(total_user < total_pc) and (total_pc <= 21):
-      answer = print("PC win")
-    elif total_user > 21:
-      answer =   print("Bust. You lose")
-    elif total_pc > 21:
-      answer =   print("Bust. You win")
-      
-    return answer 
+      answer = "You win"
+    elif (total_user < total_pc) and (total_pc <= 21):
+      answer = "PC win"
+ 
+    return print(answer)
+
+def game_hit(Ace):
+    total_pc = 0
+    total_user = 0
+    answer = ""
+
+# Stand
+    for k in range(3):
+     if user_cards[k] == "11":  
+      if Ace == True: 
+        user_cards[k] = "1"
+      else: 
+        Ace == False
+
+# Add 2 cards fro user together and add 3 cards from dealer. 
+    for i in range(3):
+      total_pc += int(pc_cards[i])
+    for k in range(2):
+      total_user += int(user_cards[i])
+
+# Check who has the closest to 21 or bust
+    if (total_pc < 21) or (total_user < 21):
+     answer = " Busted! "
+
+    elif (total_user > total_pc) and (total_user <= 21):
+      answer = "You win"
+    elif (total_user < total_pc) and (total_pc <= 21):
+      answer = "PC win"
+   
+    
+ 
+ 
+    return print(answer)
+  
   
 flag = False  
 while flag == False:
  choice = input("Would you like to play Black Jack? y/n").lower()
  if choice == 'y':
   for i in range(3):
-   hand_pc = random.choice(cards)
+   hand_pc = random.choice(cards)  # Generates a set of 3 random cards for the cards list above
    pc_cards.append(hand_pc)
 
   for i in range(3):
-   hand_user = random.choice(cards)
+   hand_user = random.choice(cards)   # Generates a set of 3 random cards for the cards list above
    user_cards.append(hand_user)
+
+ # Shows the user both 2 of their and the pc/dealers cards
   print(f"The computer got: [ {pc_cards[0]} , {pc_cards[1]}]")        
-  print(f"You got: [ {pc_cards[0]} , {pc_cards[1]}]")  
-  choice2 = print("Would you like to hit or stand? h/s")
-  if choice2 == "h":
+  print(f"You got: [ {user_cards[0]} , {user_cards[1]}]")     
+
+ # If within user cards a card valued 11 if found, they will be asked whether to change the value of that card to an ace/jack
+  if (user_cards[0] == "11") or  (user_cards[1] == "11"):  
+    ace_choice = input("Would you like to change the value to an ace or keep it as a jack? a/j").lower()
+    if ace_choice == 'a':
+      Ace = True
+    else:
+      Ace = False
+
+
+ choice2 = input("Would you like to hit or stand? h/s")  # Ask to hit or stand
+ if choice2 == "s":                                   #if to hit, an additional card would be visible for both parties -> game() function will calc who busted/loss
+    print(f"You got: [ {pc_cards[0]} , {pc_cards[1]} , {pc_cards[2]}]")  
+    print(f"You got: [ {user_cards[0]} , {user_cards[1]} ]")  
+
+    game_stand(Ace)
+
+ if choice2 == "h":                                   #if to hit, an additional card would be visible for both parties -> game() function will calc who busted/loss
     print(f"You got: [ {pc_cards[0]} , {pc_cards[1]} , {pc_cards[2]}]")  
     print(f"You got: [ {user_cards[0]} , {user_cards[1]} , {user_cards[2]}]")  
-    game()
- else:
-     flag = False               
-            
-import random
-cards = ["11","2","3","4","5","6","7","8","9","10","10","10","10"]
-pc_cards = []
-user_cards = []
+    if (user_cards[2] == "11") :  
+     ace_choice= input("Would you like to change the value to an ace or keep it as a jack? a/j").lower()
+     if ace_choice == 'a':
+      Ace = True
+     else:
+      Ace = False
 
-def game():
-    total_pc = 0
-    total_user =0
-    for i in range(3):
-      total_pc += int(pc_cards[i])
-    for k in range(3):
-      total_user += int(user_cards[i])
-    if (total_user > total_pc) and (total_user <= 21):
-      answer =  print("You win")
-    elif(total_user < total_pc) and (total_pc <= 21):
-      answer = print("PC win")
-    elif total_user > 21:
-      answer =   print("Bust. You lose")
-    elif total_pc > 21:
-      answer =   print("Bust. You win")
-      
-    return answer 
-  
-flag = False  
-while flag == False:
- choice = input("Would you like to play Black Jack? y/n").lower()
- if choice == 'y':
-  for i in range(3):
-   hand_pc = random.choice(cards)
-   pc_cards.append(hand_pc)
+    game_hit(Ace)
 
-  for i in range(3):
-   hand_user = random.choice(cards)
-   user_cards.append(hand_user)
-  print(f"The computer got: [ {pc_cards[0]} , {pc_cards[1]}]")        
-  print(f"You got: [ {pc_cards[0]} , {pc_cards[1]}]")  
-  choice2 = print("Would you like to hit or stand? h/s")
-  if choice2 == "h":
-    print(f"You got: [ {pc_cards[0]} , {pc_cards[1]} , {pc_cards[2]}]")  
-    print(f"You got: [ {user_cards[0]} , {user_cards[1]} , {user_cards[2]}]")  
-    game()
- else:
-     flag = False            
+else:
+     flag = True
