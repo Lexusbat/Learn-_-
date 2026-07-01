152e09528786e750fda700cd7c1bb7ca37b5fc33
Lexusbat <skye.lexus205@gmail.com>
2026-07-01 14:46:45 +0200

    Functional: Works well. Conditions almost met

--- Guess the Number/code.py
+++ Guess the Number/code.py
@@ -8,7 +8,9 @@ image = """
  //  \ ___ \\ \__/ //  // ___)_   __/  \\   __/  \\          |.  |    //  __  \\  // ___)_     |.  \    \. | \\ \__/ // |: \.        |(|  _  \\  // ___)_  //      /  
 (:   _(  _|/\\ __ //\ (:      "| /" \   :) /" \   :)         \:  |   (:  (  )  :)(:      "|    |    \    \ | /\\ __ //\ |.  \    /:  ||: |_)  :)(:      "||:  __   \  
  \_______)(__________) \_______)(_______/ (_______/           \__|    \__|  |__/  \_______)     \___|\____\)(__________)|___|\__/|___|(_______/  \_______)|__|  \___) 
-                                                                                                                                                                      """
+                                                                                                                                                                     
+                                                                                                                                                                      
+ """
 
 print("Welcome to the Guess te Number Game!!")
 
@@ -41,6 +43,7 @@ def high(remainder):
 flag = False
 while flag == False:
     num = random.randint(0, 101)
+    print(num)
     remainder = 0
 
     choice = input("Would you like to play? y/n").lower()
@@ -53,26 +56,45 @@ while flag == False:
          print("invalid input")
         elif Dif == "e":
            attempts = 10
-           while attempts != 0 :
-            print("You have {attempts} attempts to guess the number")
-            guess = input("Make a gues: ")
+           flag2 = False
+           while flag2 == False :
+            print(f"You have {attempts} attempts to guess the number. ")
+            guess = int(input("Make a guess: "))
             attempts -= 1
+            if num == guess :
+              print("Wow you got it right!")
+              flag2 = True
+            elif attempts == 0:
+               print("You Lose! Guess better!!")
+               flag2 = True
+
             if num > guess :
               remainder = num - guess
               low(remainder)
             elif num < guess :
               remainder = guess - num
               high(remainder)
-            elif num == guess :
-              "Wow you go tit right!"
+            
+        elif Dif == "h":
+           attempts = 5
+           flag2 = False
+           while flag2 == False :
+            print(f"You have {attempts} attempts to guess the number. ")
+            guess = int(input("Make a guess: "))
+            attempts -= 1
+            if num == guess :
+              print("Wow you got it right!")
+              flag2 = True
+            elif attempts == 0:
+               print("You Lose! Guess better!!")
+               flag2 = True
+
+            if num > guess :
+              remainder = num - guess
+              low(remainder)
+            elif num < guess :
+              remainder = guess - num
+              high(remainder)
               
-              
-           
-
-
-
-
-
-
-
+            
     else: flag = True
