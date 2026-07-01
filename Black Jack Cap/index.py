b2f646cd4fe10d61017fefa03fdbe4717d60ce6b
Lexusbat <skye.lexus205@gmail.com>
2026-07-01 12:21:58 +0200

    Functional code - Calc and outcome of game fixed

--- Black Jack Cap/index.py
+++ Black Jack Cap/index.py
@@ -3,12 +3,15 @@ cards = ["11","2","3","4","5","6","7","8","9","10","10","10","10"]
 pc_cards = []
 user_cards = []
 Ace = False # Stays 11, if True value is 1
+
 global answer 
 
 def game_stand(Ace):
     total_pc = 0
     total_user =0
     answer = ""
+    remainder_pc = 0 
+    remainder_user = 0 
 # Stand
     for k in range(2):
      if user_cards[k] == "11":
@@ -24,10 +27,21 @@ def game_stand(Ace):
       total_user += int(user_cards[i])
 
 # Check who has the closest to 21 or bust
-    if (total_user > total_pc) and (total_user <= 21):
-      answer = "You win"
-    elif (total_user < total_pc) and (total_pc <= 21):
-      answer = "PC win"
+
+    
+    if (total_pc <= 21) and (total_user <= 21) :
+      remainder_pc = 21 - total_pc
+      remainder_user = 21 - total_user
+      min( remainder_user,  remainder_pc)
+      if total_pc == min( remainder_user,  remainder_pc):
+        answer = "PC wins"
+      else: answer = "You win"
+    elif (total_pc > 21) and (total_user <= 21):
+      answer = "PC BUSTED. You win "
+    elif (total_user > 21) and (total_pc <= 21):
+      answer = "YOU BUSTED. PC wins "
+    elif (total_user > 21) and (total_pc > 21):
+      answer = "YOU BUSTED. PC BUSTED. DRAW " 
  
     return print(answer)
 
@@ -35,6 +49,8 @@ def game_hit(Ace):
     total_pc = 0
     total_user = 0
     answer = ""
+    remainder_pc = 0 
+    remainder_user = 0 
 
 # Stand
     for k in range(3):
@@ -51,17 +67,22 @@ def game_hit(Ace):
       total_user += int(user_cards[i])
 
 # Check who has the closest to 21 or bust
-    if (total_pc < 21) or (total_user < 21):
-     answer = " Busted! "
 
-    elif (total_user > total_pc) and (total_user <= 21):
-      answer = "You win"
-    elif (total_user < total_pc) and (total_pc <= 21):
-      answer = "PC win"
-   
-    
+    if (total_pc <= 21) and (total_user <= 21) :
+      remainder_pc = 21 - total_pc
+      remainder_user = 21 - total_user
+      min( remainder_user,  remainder_pc)
+      if total_pc == min( remainder_user,  remainder_pc):
+        answer = "PC wins"
+      else: answer = "You win"
+    elif (total_pc > 21) and (total_user <= 21):
+      answer = "PC BUSTED. You win "
+    elif (total_user > 21) and (total_pc <= 21):
+      answer = "YOU BUSTED. PC wins "
+    elif (total_user > 21) and (total_pc > 21):
+      answer = "YOU BUSTED. PC BUSTED. DRAW " 
  
- 
+
     return print(answer)
   
   
@@ -90,7 +111,7 @@ while flag == False:
       Ace = False
 
 
- choice2 = input("Would you like to hit or stand? h/s")  # Ask to hit or stand
+ choice2 = input("Would you like to hit or stand? h/s").lower() # Ask to hit or stand
  if choice2 == "s":                                   #if to hit, an additional card would be visible for both parties -> game() function will calc who busted/loss
     print(f"You got: [ {pc_cards[0]} , {pc_cards[1]} , {pc_cards[2]}]")  
     print(f"You got: [ {user_cards[0]} , {user_cards[1]} ]")  
