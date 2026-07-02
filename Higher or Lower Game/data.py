27d1201ee0a839dca110e42dd30c9051be975b61
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-02 11:51:53 +0200

    Added data: Celeb data in List

---
+++ Higher or Lower Game/data.py
@@ -0,0 +1,152 @@
+celebrities = [
+    {
+        "name": "Cristiano Ronaldo",
+        "profession": "Professional footballer",
+        "followers_millions": 665,
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "profession": "Professional footballer",
+        "followers_millions": 507,
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "profession": "Singer, actress, entrepreneur",
+        "followers_millions": 405,
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "profession": "Media personality, businesswoman",
+        "followers_millions": 382,
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "profession": "Actor, former wrestler",
+        "followers_millions": 382,
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "profession": "Singer, actress",
+        "followers_millions": 363,
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "profession": "Media personality, entrepreneur",
+        "followers_millions": 344,
+        "country": "United States"
+    },
+    {
+        "name": "Beyoncé",
+        "profession": "Singer, songwriter",
+        "followers_millions": 300,
+        "country": "United States"
+    },
+    {
+        "name": "Khloé Kardashian",
+        "profession": "TV personality, entrepreneur",
+        "followers_millions": 292,
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "profession": "Singer",
+        "followers_millions": 287,
+        "country": "Canada"
+    },
+    {
+        "name": "Kendall Jenner",
+        "profession": "Model, media personality",
+        "followers_millions": 278,
+        "country": "United States"
+    },
+    {
+        "name": "Virat Kohli",
+        "profession": "Professional cricketer",
+        "followers_millions": 271,
+        "country": "India"
+    },
+    {
+        "name": "Taylor Swift",
+        "profession": "Singer-songwriter",
+        "followers_millions": 250,
+        "country": "United States"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "profession": "Singer, actress",
+        "followers_millions": 247,
+        "country": "United States"
+    },
+    {
+        "name": "Neymar Jr.",
+        "profession": "Professional footballer",
+        "followers_millions": 228,
+        "country": "Brazil"
+    },
+    {
+        "name": "Nicki Minaj",
+        "profession": "Rapper, singer",
+        "followers_millions": 225,
+        "country": "Trinidad and Tobago"
+    },
+    {
+        "name": "Miley Cyrus",
+        "profession": "Singer, actress",
+        "followers_millions": 212,
+        "country": "United States"
+    },
+    {
+        "name": "Katy Perry",
+        "profession": "Singer-songwriter",
+        "followers_millions": 201,
+        "country": "United States"
+    },
+    {
+        "name": "Zendaya",
+        "profession": "Actress, singer",
+        "followers_millions": 180,
+        "country": "United States"
+    },
+    {
+        "name": "Kevin Hart",
+        "profession": "Comedian, actor",
+        "followers_millions": 177,
+        "country": "United States"
+    },
+    {
+        "name": "Cardi B",
+        "profession": "Rapper",
+        "followers_millions": 163,
+        "country": "United States"
+    },
+    {
+        "name": "LeBron James",
+        "profession": "Professional basketball player",
+        "followers_millions": 160,
+        "country": "United States"
+    },
+    {
+        "name": "Demi Lovato",
+        "profession": "Singer, actress",
+        "followers_millions": 152,
+        "country": "United States"
+    },
+    {
+        "name": "Rihanna",
+        "profession": "Singer, entrepreneur",
+        "followers_millions": 150,
+        "country": "Barbados"
+    },
+    {
+        "name": "Shakira",
+        "profession": "Singer-songwriter",
+        "followers_millions": 93,
+        "country": "Colombia"
+    }
+]
