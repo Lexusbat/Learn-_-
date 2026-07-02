a262abc3ca13ac5151131e3db6a6264077d9ec59
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-02 15:42:10 +0200

    More debugging

--- Higher or Lower Game/code.py
+++ Higher or Lower Game/code.py
@@ -2,21 +2,21 @@ import random #Import random module
 from logo import load_logo, load_vs #Imports imagaes from logo.py
 from data import celebrities #Imports data from data.py
 global right_option_celeb  
-
+celeb_compare = []
 
 load_logo()
 
 
 def get_random_celebrity():
-    comparison_celebrityA_info = ""
-    comparison_celebrityA_followers = 0
+    comparison_celebrity_info = ""
+    comparison_celebrity_followers = 0
     """Returns a random celebrity from the data list."""
-    comparison_celebrityA = random.choice(celebrities)
-    comparison_celebrityA_info = f"{comparison_celebrityA['name']}, a {comparison_celebrityA['profession']} from {comparison_celebrityA['country']} "
-    comparison_celebrityA_followers = comparison_celebrityA['followers_millions']
+    comparison_celebrity = random.choice(celebrities)
+    comparison_celebrity_info = f"{comparison_celebrity['name']}, a {comparison_celebrity['profession']} from {comparison_celebrity['country']} "
+    comparison_celebrity_followers = comparison_celebrity['followers_millions']
 
-    return comparison_celebrityA_info,comparison_celebrityA_followers
-    
+    return comparison_celebrity_info,comparison_celebrity_followers
+
 def get_random_celebrity2():
     comparison_celebrityB_info = ""
     comparison_celebrityB_followers = 0
@@ -41,54 +41,69 @@ if choice not in ['y', 'n']:
 elif choice == 'y':
    print("Great! Let's get started!")
    right_option_celeb = ""
-   while flag_game == False:
+#CREATING 2 CELEB ACCOUNTS WITH ON FUNCTION
+   comparison_celebrity_info, comparison_celebrity_followers = get_random_celebrity()
+   celebAinfo = comparison_celebrity_info
+   celebAfollowers = comparison_celebrity_followers
+   comparison_celebrity_info, comparison_celebrity_followers = get_random_celebrity()
+   celebBinfo = comparison_celebrity_info
+   celebBfollowers = comparison_celebrity_followers
+   celeb_compare.append(celebAinfo, celebBinfo)
+   
 
-
-
-
-    # Continue with the game logic here
-    comparison_celebrityA_info, comparison_celebrityA_followers = get_random_celebrity()
-    comparison_celebrityB_info, comparison_celebrityB_followers = get_random_celebrity2()
-    max_followers = 0
-    score = 0
-    print("Comparison A: ") 
-    print(comparison_celebrityA_info)
-    load_vs()
-    print("Comparison B: ")
-    
-    if get_random_celebrity2() == get_random_celebrity():
-        print(comparison_celebrityB_info)
+   flag = False
+   max_followers = 0
+   score = 0
+   print("Comparison A: ") 
+   print(celebAinfo)
+   load_vs() 
+   print("Comparison B: ")
+   while flag == False:
+    if celebBinfo == celebAinfo:
+        print(celebBinfo)
         flag = True
-    else: print(comparison_celebrityB_info)
-      
-    """ Max variable checks which A/B option has most followers, then compare it with the option chosen from user -
+    else: print(celebBinfo) and  flag = True
+
+
+   max_followers = max(celebAinfo,celebBinfo)
+
+
+   """ Max variable checks which A/B option has most followers, then compare it with the option chosen from user -
         If max_followers == followers amount == option chosen via guess variable, then increase score variable, else end game.
     """
-    max_followers = max(comparison_celebrityA_followers,comparison_celebrityB_followers)
-    guess = input("Guess, who has more followers? A/B").lower()
-    if guess not in ["a","b"]:
+   guess = input("Guess, who has more followers? A/B").lower()
+   if guess not in ["a","b"]:
        print("Invalid Answer, remember 'A' or 'B'")
        guess = input("Guess, who has more followers? A/B").lower()
-    elif guess == 'a':
-       if max_followers != comparison_celebrityA_followers:
-        print(f"Wrong answer! That's game! \n Your Final Score is: {score}")
+
+   elif guess == 'a':
+       if max_followers != celebAinfo:
+        print(f"Wrong answer! That's game! \n++Your Final Score is: {score}++")
         end_game += 1
-       elif max_followers == comparison_celebrityA_followers:
+       elif max_followers == celebAinfo:
           print("Lots of followers! You are correct!")
           score += 1
-          right_option_celeb = comparison_celebrityA_info
+          right_option_celeb = celebAinfo
+          celeb_compare[0] = right_option_celeb
+          celeb_compare[1] = ""
           print(f"Your Score is: {score}")
-
-    elif guess == 'b':
-       if max_followers != comparison_celebrityB_followers:
-        print(f"Wrong answer! That's game! \n Your Final Score is: {score}")
+   elif guess == 'b':
+       if max_followers != celebBinfo:
+        print(f"Wrong answer! That's game! \n++Your Final Score is: {score}++")
         end_game += 1
-       elif max_followers == comparison_celebrityB_followers:
+       elif max_followers == celebBinfo:
           print("Lots of followers! You are correct!")
           score += 1
-          right_option_celeb = comparison_celebrityB_info
+          right_option_celeb = celebBinfo
           print(f"Your Score is: {score}")
-
+          
+   while flag_game == False:
+    
+    if right_option_celeb== comparison_celebrity_info:
+        print(right_option_celeb)
+        flag = True
+    else: print(right_option_celeb)
+      
     if end_game == 1:
         flag_game = True
 
