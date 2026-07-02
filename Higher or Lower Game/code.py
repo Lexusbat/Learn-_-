73cf6a05d82046b8d4cb48327fb4b1dcd402dc3e
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-02 13:40:53 +0200

    Max Variable edited & additional while loop + Flag Variable - Basic game play possible.

--- Higher or Lower Game/code.py
+++ Higher or Lower Game/code.py
@@ -1,7 +1,7 @@
 import random #Import random module
 from logo import load_logo, load_vs #Imports imagaes from logo.py
 from data import celebrities #Imports data from data.py
-global comparison_celebrityA
+global right_option_celeb
 
 
 load_logo()
@@ -9,7 +9,7 @@ load_logo()
 
 def get_random_celebrity():
     comparison_celebrityA_info = ""
-    comparison_celebrityA_followers = ""
+    comparison_celebrityA_followers = 0
     """Returns a random celebrity from the data list."""
     comparison_celebrityA = random.choice(celebrities)
     comparison_celebrityA_info = f"{comparison_celebrityA['name']}, a {comparison_celebrityA['profession']} from {comparison_celebrityA['country']} "
@@ -19,7 +19,7 @@ def get_random_celebrity():
     
 def get_random_celebrity2():
     comparison_celebrityB_info = ""
-    comparison_celebrityB_followers = ""
+    comparison_celebrityB_followers = 0
     """Returns a random celebrity from the data list."""
     # Ensure that the second celebrity is not the same as the first one
     comparison_celebrityB = random.choice(celebrities)
@@ -34,51 +34,58 @@ print("Welcome to Higher vs Lower game!")
 print("The goal is to compare celebs Instagram followers. Guess the one with the moste followers and lets see how many point you can score!")
 choice = input("Are you ready to play? Type 'y' for yes or 'n' for no: ").lower()
 flag = False
+flag_game = False
 
 if choice not in ['y', 'n']:
     print("Invalid input. Please enter 'y' or 'n'.")
 elif choice == 'y':
+   
+
+   while flag_game == False:
+  
+
     print("Great! Let's get started!")
     # Continue with the game logic here
     comparison_celebrityA_info, comparison_celebrityA_followers = get_random_celebrity()
     comparison_celebrityB_info, comparison_celebrityB_followers = get_random_celebrity2()
-    max = 0
+    max_followers = 0
     score = 0
-
     print("Comparison A: ") 
     print(comparison_celebrityA_info)
     load_vs()
     print("Comparison B: ")
-    while flag == False:
-     if get_random_celebrity2() == get_random_celebrity():
+    
+    if get_random_celebrity2() == get_random_celebrity():
         print(comparison_celebrityB_info)
         flag = True
-     else: print(comparison_celebrityB_info)
-     exit()
+    else: print(comparison_celebrityB_info)
+      
     """ Max variable checks which A/B option has most followers, then compare it with the option chosen from user -
-        If max == followers amount == option chosen via guess variable, then increase score variable, else end game.
+        If max_followers == followers amount == option chosen via guess variable, then increase score variable, else end game.
     """
-    max = max(comparison_celebrityA_followers,comparison_celebrityB_followers)
+    max_followers = max(comparison_celebrityA_followers,comparison_celebrityB_followers)
     guess = input("Guess, who has more followers? A/B").lower()
     if guess not in ["a","b"]:
        print("Invalid Answer, remember 'A' or 'B'")
        guess = input("Guess, who has more followers? A/B").lower()
     elif guess == 'a':
-       if max == comparison_celebrityA_followers:
+       if max_followers == comparison_celebrityA_followers:
           print("Lots of followers! You are correct!")
           score += 1
        else: print("Wrong answer! That's game!")
+       print(str(score))
+       flag_game = True
+
     elif guess == 'b':
-       if max == comparison_celebrityB_followers:
+       if max_followers == comparison_celebrityB_followers:
           print("Lots of followers! You are correct!")
           score += 1
        else: print("Wrong answer! That's game!")
-
-
-
-
-
-
+       print(str(score))
+       flag_game = True
+   else: 
+    exit()
+       
 
 elif choice == 'n':
     print("No worries! Come back when you're ready. Goodbye!")
