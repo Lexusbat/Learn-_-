ae79b5d4ae91d1511fc19a1715ae087fbe4bc1d6
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-03 14:49:24 +0200

    Methods to  Move object

--- Sammy's Adventure/main.py
+++ Sammy's Adventure/main.py
@@ -6,9 +6,27 @@ Sammy = Turtle()
 # Attributes for Sammy (Object)
 print(Sammy)
 Sammy.shape("turtle")
+Sammy.color("DeepPink2")
+Sammy.left(90)
+Sammy.forward(350)
+Sammy.left(90)
+Sammy.forward(10)
+Sammy.left(90)
+Sammy.forward(600)
+Sammy.left(90)
+Sammy.forward(20)
+Sammy.left(90)
+Sammy.forward(600)
+Sammy.left(90)
+Sammy.forward(10)
+Sammy.home()
+
+
+
+
 
 my_screen = Screen()
-
+#Attribute and Method for Tkinter canva to stay on screen
 print(my_screen.canvheight)
-my_screen.exitonclick
+my_screen.exitonclick()
 
