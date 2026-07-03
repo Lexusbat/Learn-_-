32784ae59b65eaf3e811efdcdfec52f6fa317cb4
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-03 14:31:30 +0200

    Additional Class & Object with attribute/methods

--- Sammy's Adventure/main.py
+++ Sammy's Adventure/main.py
@@ -1,3 +1,14 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+import tkinter as TK
 
+# Object = Class()
 Sammy = Turtle()
+# Attributes for Sammy (Object)
+print(Sammy)
+Sammy.shape("turtle")
+
+my_screen = Screen()
+
+print(my_screen.canvheight)
+my_screen.exitonclick
+
