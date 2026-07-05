0f61ec0a1f44b7ce8f451b80e969cab801098ff5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-05 16:29:15 +0200

    Code and implementation of Class & Obkects already made

--- OOP Coffee Machine/main.py
+++ OOP Coffee Machine/main.py
@@ -1,3 +1,58 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+flag = False
+money = 0 
+while flag == False:
+    request = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if request not in ["e","l","c","report","exit"]:
+        print("Invalid input.Try again")
+        print("Type in either 'e'/'l'/'c' please: \n")
+    elif request == "report":
+     CoffeeMaker.report()
+     print(f"Money: ${money}")
+    elif request == "e":
+       request = "espresso"
+       cost = MenuItem.name 
+       check = CoffeeMaker.is_resource_sufficient(request)
+       if check == True:
+        print("Please insert coins: ")
+        MoneyMachine.make_payment()
+        money = money + money_add
+        if insufficient == 0 :
+         process_order(request)
+        else:
+         print("xxxxxxxxxxxxxxxxxxxxx")
+           
+
+    elif request == "l":
+       request = "latte"
+       if check_resources(request) == True:
+        print("Please insert coins: ")
+        money_add, insufficient = calc_transaction(request)       
+        money = money + money_add
+        if insufficient == 0 :
+         process_order(request)
+        else:
+         print("xxxxxxxxxxxxxxxxxxxxx")
+           
+
+    elif request == "c":
+       request = "cappuccino"
+       if check_resources(request) == True:
+        print("Please insert coins: ")
+        money_add, insufficient = calc_transaction(request)       
+        money = money + money_add
+        if insufficient == 0 :
+         process_order(request)
+        else:
+         print("xxxxxxxxxxxxxxxxxxxxx")
+           
+
+       
+    elif request == "exit":
+       flag = True
+
+       
