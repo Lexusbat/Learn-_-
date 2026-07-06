74a8a2f039015f4815c35531f9db93cfc3fb28a2
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-06 10:51:42 +0200

    Creating Objects for thier classes

--- OOP Coffee Machine/main.py
+++ OOP Coffee Machine/main.py
@@ -1,7 +1,10 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 flag = False
 money = 0 
