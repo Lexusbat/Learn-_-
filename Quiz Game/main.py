756dfc326f94c6228140292d5c7976d995ade83f
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-06 17:03:35 +0200

    Init Method Initialised & More objects created

--- Quiz Game/main.py
+++ Quiz Game/main.py
@@ -1,5 +1,19 @@
 print("Hello! Welcome to the Quiz Game!")
 
 class User: #Creation of a Class
-    pass #Close Class Block. Can be done with Functions too.
+    def __init__(self): #Initialise attributes (Method)
+     #NOTE: Everytime user = User() is created, the following happens
+     print("New user being created...")
 
+     pass #Close Class Block. Can be done with Functions too.
+
+#Object = Class (created above)
+user_1 = User()
+user_2 = User()
+user_3 = User()
+"""
+Output:
+New user being created...
+New user being created...
+New user being created...
+"""
