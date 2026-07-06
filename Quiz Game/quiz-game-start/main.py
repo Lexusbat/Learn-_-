b3387d8947bebf9588e32b95faf10d42410b6084
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-06 19:01:27 +0200

    Additional Methods, Objects and Class created

--- Quiz Game/quiz-game-start/main.py
+++ Quiz Game/quiz-game-start/main.py
@@ -1,5 +1,7 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 
@@ -15,3 +17,6 @@ for question in question_data:
     question_bank.append(new_question)
 
 print(question_bank)
+
+quiz = QuizBrain()
+quiz.next_question()
