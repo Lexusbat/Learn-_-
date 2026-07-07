c4f4737dca6943074f570f9795290e6b9f22f80e
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-07 16:15:08 +0200

    Code with methods/functions: Dash Line

--- GUI Turtle/main.py
+++ GUI Turtle/main.py
@@ -3,5 +3,18 @@ from turtle import Turtle, Screen
 la_turtle = Turtle()
 
 
+"""
+for i in range(10):
+ la_turtle.forward(10)
+ la_turtle.penup()
+ la_turtle.forward(10)
+ la_turtle.pendown()
+ la_turtle.forward(10)
+
+"""
+
+
+
+
 my_screen = Screen()
 my_screen.exitonclick()
