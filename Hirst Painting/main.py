9808778116c453ab8018609bf5766fe301ad4145
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-08 13:35:25 +0200

    Additional code and debugging

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -1,13 +1,7 @@
 import colorgram
-colors = colorgram.extract("Skeleton.jpg", 6)
-first_color = colors[0]
-rgb = first_color.rgb
-hsl = first_color.hsl 
-proportion  = first_color.proportion
-red = rgb[0]
-red = rgb.r
-saturation = hsl[1]
-saturation = hsl.s
 
-colorgram.Color
-sorted(colors, key=lambda c: c.hsl.h)
+# Extract 6 colors from the image
+colors = colorgram.extract("Skeleton.jpg", 30)
+
+for color in colors:
+    print(color.rgb)
