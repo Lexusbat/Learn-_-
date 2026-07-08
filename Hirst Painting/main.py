d124100fc984e82c5bbb2323776044f631b12796
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-08 13:30:53 +0200

    Additional debuiugging: JPEG

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -1,3 +1,13 @@
 import colorgram
-colors = colorgram.extract('Skeleton', 6)
+colors = colorgram.extract("Skeleton.jpg", 6)
+first_color = colors[0]
+rgb = first_color.rgb
+hsl = first_color.hsl 
+proportion  = first_color.proportion
+red = rgb[0]
+red = rgb.r
+saturation = hsl[1]
+saturation = hsl.s
+
 colorgram.Color
+sorted(colors, key=lambda c: c.hsl.h)
