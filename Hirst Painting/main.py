73568313646ecba2d0b23bb274b3b047a5db7ff6
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-08 13:40:10 +0200

    List & Tuple: Store rgb values of 30 colors

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -2,6 +2,11 @@ import colorgram
 
 # Extract 6 colors from the image
 colors = colorgram.extract("Skeleton.jpg", 30)
-
+rgb_colors = []
 for color in colors:
-    print(color.rgb)
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_colour = (r,g,b)
+    rgb_colors.append(new_colour)
+print(rgb_colors)
