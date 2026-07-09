37351e294a8f8d6b96224fe273b26983c32fc0a5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 16:29:33 +0200

    Set Object Position

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -1,11 +1,13 @@
 from turtle import Turtle, Screen
 
-sam = Turtle()
+
 screen = Screen()
 
-
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win? Bet on it! Choose a colour: ")
 
-
+sam = Turtle()
+sam.penup()
+sam.goto(x=-200,y=0)
 
 screen.exitonclick()
