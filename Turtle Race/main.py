10b2ebb4ecf61fe9b0f779a1984f2e0980b86c64
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 17:02:08 +0200

    Additonal code and debugging

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -4,6 +4,18 @@ import random
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win? Bet on it! Choose a colour: ")
+colors = ["black","red","blue","purple","yellow","green"] # 6 color turtles
+y_pos = [-70,-40,-10,20,50,80]
+
+
+sam = Turtle()
+
+def Turtle_pos(index):
+    sam.shape("turtle")
+    sam.color(colors[index])
+    sam.goto(-200,y_pos)
+
+"""
 
 def race():
     spaces_sam  = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
@@ -22,6 +34,11 @@ tim.penup()
 tim.goto(x=-200,y=-50)
 while (sam.position() != (250.00, 0.00)) and (tim.position() != (250.00, -50.00)) :
     race()
+
+"""
+for index in range(0,5):
+    Turtle_pos(index)
+
 
 
 screen.exitonclick()
