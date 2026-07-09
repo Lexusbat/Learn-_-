951c2037ad71c214b1aeea94564f79e3f31e919f
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 16:40:06 +0200

    Debug randint

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -6,7 +6,7 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win? Bet on it! Choose a colour: ")
 
 def race():
-    speed = random.randint(range(0,11))
+    speed = random.randint(0, 10)   # Returns an integer from 0 to 10 (inclusive)
     sam.speed(speed)
     sam.forward(10)
 
@@ -14,7 +14,8 @@ def race():
 sam = Turtle()
 sam.penup()
 sam.goto(x=-200,y=0)
-
+while sam.position() != (250.00, 0.00):
+    race()
 
 
 screen.exitonclick()
