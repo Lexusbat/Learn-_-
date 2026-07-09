994a7a234305c276a97718dde766d210e21a7cb4
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 16:49:40 +0200

    2 Opponents/Objects

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -6,15 +6,21 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win? Bet on it! Choose a colour: ")
 
 def race():
-    speed = random.randint(0, 10)   # Returns an integer from 0 to 10 (inclusive)
-    sam.speed(speed)
-    sam.forward(10)
+    spaces_sam  = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
+    sam.speed(1)
+    sam.forward(spaces_sam)
+    spaces_tim = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
+    tim.speed(1)
+    tim.forward(spaces_tim)
 
 
 sam = Turtle()
+tim = Turtle()
 sam.penup()
 sam.goto(x=-200,y=0)
-while sam.position() != (250.00, 0.00):
+tim.penup()
+tim.goto(x=-200,y=-50)
+while (sam.position() != (250.00, 0.00)) and (tim.position() != (250.00, -50.00)) :
     race()
 
 
