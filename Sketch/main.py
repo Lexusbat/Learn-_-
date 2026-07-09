5433d40f7b746ac3941f37c0e5755ce3516cb9e5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 16:02:36 +0200

    Additional functions anti-clockwise

--- Sketch/main.py
+++ Sketch/main.py
@@ -6,15 +6,20 @@ screen = Screen()
 def move_forward():
     sam.forward(10)
 
-def clockwise_turn(increase):
-    sam.setheading(increase)
+def clockwise_turn():
+   new_heading = sam.heading() + 10
+   sam.setheading(new_heading)
+   
+def anti_clockwise_turn():
+   new_heading = sam.heading() - 10
+   sam.setheading(new_heading)
 
 def move_backwards():
     sam.backward(10)
 
 screen.listen()
 screen.onkey(key="w",fun=move_forward,)
-
+screen.onkey(key="d",fun=clockwise_turn,)
 screen.onkey(key="s",fun=move_backwards,)
 
 screen.exitonclick()
