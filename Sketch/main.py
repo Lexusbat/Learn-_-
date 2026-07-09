e6f2491d1432a13f06e87fcb5ae72206bdc0e2f5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 16:07:54 +0200

    Clear  drawing function

--- Sketch/main.py
+++ Sketch/main.py
@@ -17,9 +17,14 @@ def anti_clockwise_turn():
 def move_backwards():
     sam.backward(10)
 
+def clear():
+    sam.clear()
+
 screen.listen()
 screen.onkey(key="w",fun=move_forward)
 screen.onkey(key="d",fun=clockwise_turn)
 screen.onkey(key="s",fun=move_backwards)
 screen.onkey(key="a",fun=anti_clockwise_turn)
+screen.onkey(key="c",fun=clear)
+
 screen.exitonclick()
