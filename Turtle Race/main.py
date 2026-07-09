5a8ced6f7af08dd5ae436463b506bd1e8ec6bee5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-09 17:17:47 +0200

    List and additional code

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -6,26 +6,24 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win? Bet on it! Choose a colour: ")
 colors = ["black","red","blue","purple","yellow","green"] # 6 color turtles
 y_pos = [-70,-40,-10,20,50,80]
+all_turtles =[]
 
-
-
-def Turtle_pos(index):
+def Turtle_pos(index,spaces_sam):
     sam = Turtle()
     sam.penup()
     sam.shape("turtle")
     sam.color(colors[index])
     sam.goto(-200,y_pos[index])
+    sam.forward(spaces_sam)
+    
+is_race_on = False
 
 """
-
 def race():
     spaces_sam  = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
     sam.speed(1)
     sam.forward(spaces_sam)
     spaces_tim = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
-    tim.speed(1)
-    tim.forward(spaces_tim)
-
 
 sam = Turtle()
 tim = Turtle()
@@ -33,12 +31,16 @@ sam.penup()
 sam.goto(x=-200,y=0)
 tim.penup()
 tim.goto(x=-200,y=-50)
-while (sam.position() != (250.00, 0.00)) and (tim.position() != (250.00, -50.00)) :
-    race()
-
 """
-for index in range(6):
-    Turtle_pos(index)
+
+
+
+while is_race_on ==  False:
+    spaces_sam  = random.randint(5, 20)   # Returns an integer from 0 to 10 (inclusive)
+    for index in range(6):
+      Turtle_pos(index,spaces_sam)
+
+
 
 
 
