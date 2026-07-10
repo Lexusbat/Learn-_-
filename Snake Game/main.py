e16868a9b7878f5af1c05dbd785391fd15856d61
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-10 17:32:18 +0200

    Function snake body

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -5,11 +5,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+def snake():
+ xpos = 0
+ ypos = 0
+ for  snake in range(3):
+  sam = Turtle()
+  sam.shape("square")
+  sam.pos(xpos,ypos)
+  xpos += 20
 
 
 
-
-
+snake()
 
 
 
