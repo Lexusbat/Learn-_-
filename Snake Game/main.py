dfb92fe8d0480c4a45fde07a90a02534b1a72186
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-10 17:46:31 +0200

    While Loop: Movement

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -7,22 +7,31 @@ screen.title("My Snake Game")
 
 snake_parts = []
 
-def snake():
+def snake_body():
  xpos = 0.00
  ypos = 0.00
  for  snake in range(3):
   snake_part = Turtle()
+  snake_part.penup()
   snake_part.color("white")
   snake_part.shape("square")
   snake_parts.append(snake_part)
   snake_part.setpos(xpos,ypos)
   xpos += 20.00
+ 
+
+snake_body()
+
+is_game_on = True
 
 
+while is_game_on == True:
 
+ for snake in snake_parts:
+  snake.forward(10)
+  snake.penup()
 
-snake()
-
+ 
 
 
 
