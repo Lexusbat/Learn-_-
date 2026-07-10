59757a0d753c16ac8e54596f4fdfe16b8e54f9bb
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-10 18:02:17 +0200

    Additional code

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,8 +1,9 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, Time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 screen.title("My Snake Game")
 
 snake_parts = [(0.00,0.00),(-20.00,0.00),(-40.00,0.00)]
@@ -21,15 +22,13 @@ snake_body()
 
 is_game_on = True
 
-"""
+
 while is_game_on == True:
+ screen.update()
+ time.sleep(-1)
 
  for snake in snake_parts:
-  snake.forward(10)
-  snake.left(90)
-  snake.penup()
 
- """
 
 
 
