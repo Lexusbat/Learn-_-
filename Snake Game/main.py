521dab539feb04783640a0cbc8f312ba665896fe
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-10 17:55:23 +0200

    Upgraded function code: Debugging

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -8,23 +8,20 @@ screen.title("My Snake Game")
 snake_parts = [(0.00,0.00),(-20.00,0.00),(-40.00,0.00)]
 
 def snake_body():
- xpos = 0.00
- ypos = 0.00
- for  snake in range(3):
+ for  positions in snake_parts:
   snake_part = Turtle()
   snake_part.penup()
   snake_part.color("white")
   snake_part.shape("square")
-  snake_parts.append(snake_part)
-  snake_part.setpos(xpos,ypos)
-  xpos += 20.00
+  snake_part.goto(positions)
+
  
 
 snake_body()
 
 is_game_on = True
 
-
+"""
 while is_game_on == True:
 
  for snake in snake_parts:
@@ -32,7 +29,7 @@ while is_game_on == True:
   snake.left(90)
   snake.penup()
 
- 
+ """
 
 
 
