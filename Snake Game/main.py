49de903af7809384fafaf773e8c7c98e91f9ed6c
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-13 16:26:34 +0200

    Additonal code tweaks and cleanup + debug

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -2,8 +2,6 @@ from turtle import Turtle, Screen
 import time
 from snake import Snake
 
-start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
-snake_parts = [] #List of all snake part (3)- will be able tc control form this list
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,7 +10,7 @@ screen.tracer(0)
 screen.title("My Snake Game")
 
 """Creates Snake - __init__"""
-snake = Snake(start_pos,snake_parts)
+snake = Snake()
 
 is_game_on = True
 
@@ -20,7 +18,7 @@ is_game_on = True
 while is_game_on:
  screen.update()
  time.sleep(0.05)      
- snake.move(snake_parts) 
+ snake.move() 
 
 
  #screen.onkey(key="d",fun=clockwise_turn)
--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,10 +1,11 @@
 from turtle import Turtle
+
 class Snake: 
     start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
     snake_parts = [] #List of all snake part (3)- will be able tc control form this list
 
 
-    def __init__(self,start_pos,snake_parts):
+    def __init__(self):
        """Creates Snake"""
        for  positions in self.start_pos:
         self.snake_part = Turtle("square")
@@ -15,10 +16,10 @@ class Snake:
 
         pass
 
-    def move(self,snake_parts):
+    def move(self):
        new_x = 0
        new_y = 0
-       """Moves Snake"""                      #2       
+       """Moves Snake"""   
        for snake_num in range(len(self.snake_parts)- 1, 0, -1 ): # start= 1,stop= 3 , step= 1 BUT Check above tuple (backwards)
          new_x = self.snake_parts[snake_num - 1].xcor()
          new_y = self.snake_parts[snake_num - 1].ycor()
