900eb9570eca913423eb01187375b58be7a2416a
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-13 15:39:06 +0200

    For statement changed

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -8,7 +8,7 @@ screen.tracer(0)
 screen.title("My Snake Game")
 
 snake_parts = [] #List of all snake part (3)- will be able tc control form this list
-start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple
+start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
 for  positions in start_pos:
   snake_part = Turtle("square")
   snake_part.color("white")
@@ -26,10 +26,13 @@ is_game_on = True
 
 while is_game_on:
  screen.update()
- for snake in snake_parts:
+ time.sleep(0.05)
+ for snake_num in range(start= 2,stop= 0, step= -1 ): # start= 1,stop= 3 , step= 1 BUT Check above tuple (backwards)
+  new_x = snake_parts[snake_num - 1].xcor()
+  new_y = snake_parts[snake_num - 1].xcor()
+  snake_parts[snake_num].goto(new_x, new_y)
 
-  snake.forward(20)
-  time.sleep(0.05)
+
 
 
 
