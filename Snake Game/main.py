235be3f582bf650049bd32d3fb946e057fbb47c2
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-13 15:03:35 +0200

    Code clean

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -9,10 +9,8 @@ screen.title("My Snake Game")
 snake_parts = [] #List of all snake part (3)- will be able tc control form this list
 start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple
 for  positions in start_pos:
-  snake_part = Turtle()
-  snake_part.penup()
+  snake_part = Turtle("square")
   snake_part.color("white")
-  snake_part.shape("square")
   snake_part.goto(positions)
   snake_parts.append(snake_part)
 
