f8171682ced27e55f0cfeb5e2a756e1ed9c31acd
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-13 16:16:43 +0200

    Additonal code and debugging of class

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
+start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
+snake_parts = [] #List of all snake part (3)- will be able tc control form this list
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,28 +10,21 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("My Snake Game")
 
-snake_parts = [] #List of all snake part (3)- will be able tc control form this list
-start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
-for  positions in start_pos:
-  snake_part = Turtle("square")
-  snake_part.color("white")
-  snake_part.penup()
-  snake_part.goto(positions)
-  snake_parts.append(snake_part)
-
+"""Creates Snake - __init__"""
+snake = Snake(start_pos,snake_parts)
 
 is_game_on = True
 
-
+#TODO Snake, Food, Scoreboard Classes
 while is_game_on:
  screen.update()
- time.sleep(0.05)            #2        #0       #-1
- for snake_num in range(len(snake_parts)- 1, 0, -1 ): # start= 1,stop= 3 , step= 1 BUT Check above tuple (backwards)
-  new_x = snake_parts[snake_num - 1].xcor()
-  new_y = snake_parts[snake_num - 1].ycor()
-  snake_parts[snake_num].goto(new_x, new_y)
+ time.sleep(0.05)      
+ snake.move() 
 
- snake_parts[0].forward(20)
+
+ #screen.onkey(key="d",fun=clockwise_turn)
+ #screen.onkey(key="a",fun=anti_clockwise_turn)
+
 
 
 
--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -1,24 +1,23 @@
 from turtle import Turtle
 class Snake: 
-    start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple 0,1,2
 
-    def __init__(self,snake_part,snake_parts):
+    def __init__(self,start_pos,snake_parts):
        """Creates Snake"""
-       for  positions in self.start_pos:
+       for  positions in start_pos:
         self.snake_part = Turtle("square")
-        snake_part.color("white")
-        snake_part.penup()
-        snake_part.goto(positions)
-        snake_parts.append(snake_part)
+        self.snake_part.color("white")
+        self.snake_part.penup()
+        self.snake_part.goto(positions)
+        self.snake_parts.append(self.snake_part)
 
         pass
 
-    def move():
+    def move(self,snake_parts):
        """Moves Snake"""                      #2       
-       for snake_num in range(len(snake_parts)- 1, 0, -1 ): # start= 1,stop= 3 , step= 1 BUT Check above tuple (backwards)
-         new_x = snake_parts[snake_num - 1].xcor()
-         new_y = snake_parts[snake_num - 1].ycor()
-         snake_parts[snake_num].goto(new_x, new_y)
-         snake_parts[0].forward(20)
+       for snake_num in range(len(self.snake_parts)- 1, 0, -1 ): # start= 1,stop= 3 , step= 1 BUT Check above tuple (backwards)
+         new_x = self.snake_parts[snake_num - 1].xcor()
+         new_y = self.snake_parts[snake_num - 1].ycor()
+         self.snake_parts[snake_num].goto(new_x, new_y)
+         self.snake_parts[0].forward(20)
 
        
