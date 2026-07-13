e4653b190f8be40ee3991717f08b603abfb4ca6d
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-13 15:00:47 +0200

    Debug

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -6,10 +6,9 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("My Snake Game")
 
-snake_parts = []
-start_pos =  [(0,0),(-20,0),(-40,0)]
-
-for  positions in snake_parts:
+snake_parts = [] #List of all snake part (3)- will be able tc control form this list
+start_pos =  [(0,0),(-20,0),(-40,0)] # Start pos Tuple
+for  positions in start_pos:
   snake_part = Turtle()
   snake_part.penup()
   snake_part.color("white")
@@ -26,7 +25,7 @@ is_game_on = True
 
 
 while is_game_on:
- screen.update()
+ #screen.update()
  #time.sleep(-1)
 
  for snake in snake_parts:
